19108db3182aa810b4d8ad8dab8b2ee4c2d3d1ab
Sabbir <sabbirbhuiyan026@gmail.com>
2026-03-14 02:31:49 +0600

    Applied data display from the buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,58 @@
-console.log('Test');
+const loadLessons = () => {
+    let url = "https://openapi.programming-hero.com/api/levels/all";
+    fetch(url)
+        .then((res) => res.json())
+        .then((datas) => displayLessons(datas.data));
+};
+
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((datas) => displayLevelWords(datas.data));
+};
+
+const displayLevelWords = (words) => {
+    const wordContainer = document.getElementById("word-section");
+    wordContainer.innerHTML = "";
+
+    words.forEach((word) => {
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+            <div class="space-y-6 bg-white p-14 rounded-2xl shadow-sm h-full">
+                <h2 class="font-bangla text-3xl font-bold">${word.word}</h2>
+                <p class="font-bangla text-xl font-medium">Meaning/Pronounciation</p>
+                <h2 class="font-bangla text-3xl font-bold">${word.meaning}/${word.pronunciation}</h2>
+                <div class="flex justify-between">
+                    <button class="btn rounded-xl">
+                        <i class="fa-solid fa-circle-info"></i>
+                    </button>
+                    <button class="btn rounded-xl">
+                        <i class="fa-solid fa-volume-high"></i>
+                    </button>
+                </div>
+            </div>
+        `;
+        wordContainer.appendChild(card);
+    });
+};
+
+const displayLessons = (lessons) => {
+    const btnContainer = document.getElementById("button-container");
+    btnContainer.innerHTML = "";
+
+    lessons.forEach((lesson) => {
+        console.log(lesson);
+        const btn = document.createElement("div");
+        // btn.classList.add('btn btn-outline btn-success');
+        btn.innerHTML = `
+            <button onclick = "loadLevelWord(${lesson.level_no})">
+                <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
+            </button>
+        `;
+        btn.classList.add("btn", "btn-outline", "btn-success");
+        btnContainer.appendChild(btn);
+    });
+};
+loadLessons();
